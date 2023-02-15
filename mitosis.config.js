64d69759b37d0741a0bc0fac1b67bb48fd2b35f8
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-02-15 20:46:03 +0100

    add more targets and improve setup

--- mitosis.config.js
+++ mitosis.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   files: "src/**",
-  targets: ["vue3", "solid", "svelte", "react"],
+  targets: [
+    "alpine",
+    "angular",
+    "customElement",
+    "html",
+    "mitosis",
+    "liquid",
+    "react",
+    "reactNative",
+    "solid",
+    // "svelte",
+    "swift",
+    "template",
+    "webcomponent",
+    // "vue", // defaults to vue3
+    // "vue2", // breaks generation
+    "vue3",
+    "stencil",
+    "qwik",
+    "marko",
+    "preact",
+    "lit",
+    "rsc",
+  ],
 };
--- watch.js
+++ watch.js
@@ -6,11 +6,12 @@ const ignore = ["output", "node_modules"];
 let ref;
 // One-liner for current directory
 chokidar.watch(".").on("all", (event, path) => {
+  if (path.startsWith(".")) return;
   if (ignore.some((s) => path.includes(s))) return;
   if (ref) clearTimeout(ref);
 
   ref = setTimeout(() => {
     console.log(event, path);
-    exec("pnpm build");
+    exec("pnpm build", (error, stdout, stderr) => console.log(stdout));
   }, 500);
 });
